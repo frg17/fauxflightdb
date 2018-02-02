e3d48aeb13532f3e21c3c89200bb831d804857fd
Frosti Grétarsson <frg17@hi.is>
2018-02-02 14:44:02 +0000

    Schema setup

--- ffdbessentials/database.py
+++ ffdbessentials/database.py
@@ -1,24 +1,38 @@
+"""
+Sér um gagnagrunnsaðgerðir
+"""
 import psycopg2
 from getpass import getpass
 
 def init_db(user, passw):
+    """
+    Eyðir gamla DB og býr til nýtt. 
+    Kallar á fall sem setur upp schemað
+    """
     try:
         conn = psycopg2.connect(database="postgres", user=user, password=passw)
         cur = conn.cursor()
         conn.autocommit = True
-        cur.execute("DROP DATABASE IF EXISTS FauxFlightDB")
-        cur.execute("CREATE DATABASE FauxFlightDB")
+        cur.execute("DROP DATABASE IF EXISTS fauxflightdb")
+        cur.execute("CREATE DATABASE fauxflightdb")
         cur.close()
         conn.close()
+
+        setup_schema(user, passw)
     except psycopg2.OperationalError as err:
         print("{}".format(err))
     except psycopg2.InternalError as err:
         print("{}".format(err))
 
 def setup_schema(user, passw):
+    """
+    Setur upp schemað í gagnagrunninum
+    """
     try:
-        conn = psycopg2.connect(database="FauxFlightDB", user=user, password=passw)
+        conn = psycopg2.connect(database="fauxflightdb", user=user, password=passw)
         cur = conn.cursor()
+        cur.execute(read_schema())
+        conn.commit()
         cur.close()
         conn.close()
     except psycopg2.OperationalError as err:
@@ -27,7 +41,17 @@ def setup_schema(user, passw):
         print("{}".format(err))
 
 def read_schema():
-    schemaname = "ffdbschema.sql"
+    """
+    Les inn skipanir úr ffdbschema.sql
+    til þess að setja upp gagnagrunn
+    """
+    schema = None
+    with open("./files/ffdbschema.sql") as f:
+        schema = f.read()
+    return schema
+    
+
+
 
 
 if __name__ == "__main__":
---
+++ files/ffdbschema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE flights (
+    dateof VARCHAR(50),
+    timeof VARCHAR(50),
+    origin VARCHAR(255),
+    destination VARCHAR(255),
+    traveltime INT
+);
